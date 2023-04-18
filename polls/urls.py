08d707bf9a3c43be82b279089dbbcd09a50af687
Tony <weijialin@inspur.com>
2023-04-18 17:13:19 +0800

    feat: A instance that how to generate a interface

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # str int slug uuid path is available
+    # slug Matches any slug string consisting of ASCII letters or numbers
     path("", views.index, name="index"),
+    path("<str:video_name>/", views.video, name="video"),
+    path("<str:video_name>/video_url", views.videoUrl, name="videoUrl")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Video
 
-def index(request):
-    return HttpResponse("Hello World!")
 # Create your views here.
+def index(request):
+    latest_video_list = Video.objects.order_by("-pub_data")[:5]
+    context = {"latest_video_list": latest_video_list}
+    # We don't need loader and HttpResponse with the render just like this
+    return render(request, "polls/index.html", context)
+    # return HttpResponse("Hello World!")
+
+def video(request, video_name):
+    try:
+        _video = Video.objects.get(video_name=video_name)
+    except Video.DoesNotExist:
+        raise Http404("Does not exist")
+    template = loader.get_template("polls/detail.html")
+    context = {
+        "videoInfo": _video.video_name
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("You're looking at %s." %video_name)
+
+def videoUrl(request, video_name):
+    # Don't use 'try' to catch the error, use get_object_or_404 to complete this.
+    _video = get_object_or_404(Video, video_name=video_name)
+    return render(request, "polls/detail.html", { "videoInfo": _video.video_url })
+    
