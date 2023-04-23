05851046237cb2db0a63203dd7a2c5b5b41631f1
Tony <weijialin@inspur.com>
2023-04-23 17:13:02 +0800

    feat: registe the Movie model

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Video
+from .models import Video, IYF_Movie
 
 # Register your models here.
 admin.site.register(Video)
+admin.site.register(IYF_Movie)
--- polls/models.py
+++ polls/models.py
@@ -6,3 +6,13 @@ class Video(models.Model):
     poster_url = models.CharField(max_length=200) # 海报地址
     video_url = models.CharField(max_length=200) # 海报地址
     pub_data = models.DateTimeField("date published") # 视频发布日期
+
+
+class IYF_Movie(models.Model):
+    movie_name = models.CharField(max_length=100)  # 电影名
+    movie_type = models.CharField(max_length=100)  # 电影类型
+    movie_addr = models.CharField(max_length=200)  # 影片放映地址 这个可以作为唯一标识
+    movie_time = models.CharField(max_length=50)   # 上映时间
+    movie_mark = models.CharField(max_length=10)   # 电影评分
+    movie_actor = models.CharField(max_length=100) # 演员表
+    movie_poster = models.CharField(max_length=300)# 海报地址
