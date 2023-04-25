ed2bb842ce30af31a5928f39bf0dfaf04679b49c
Tony <weijialin@inspur.com>
2023-04-25 17:56:45 +0800

    feat: xbyy and 微调

--- polls/models.py
+++ polls/models.py
@@ -14,5 +14,5 @@ class IYF_Movie(models.Model):
     movie_addr = models.CharField(max_length=200)  # 影片放映地址 这个可以作为唯一标识
     movie_time = models.CharField(max_length=50)   # 上映时间
     movie_mark = models.CharField(max_length=10)   # 电影评分
-    movie_actor = models.CharField(max_length=100) # 演员表
+    movie_actor = models.CharField(max_length=500) # 演员表
     movie_poster = models.CharField(max_length=300)# 海报地址
